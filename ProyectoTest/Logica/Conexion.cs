3b532137f91f2841892dc8049f4192e8e756bb02
JarimAlmaraz <jarimc.almaraz@gmail.com>
2022-03-18 19:20:26 -0600

    Exámen Brive

---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        public static string Get()
+        {
+            string conexion = ConfigurationManager.ConnectionStrings["DB_CARRITO"].ConnectionString;
+            return conexion;
+        }
+
+
+        //public static string CN = "Data Source=.;Initial Catalog=DB_CARRITO;Integrated Security=True";
+    }
+}
